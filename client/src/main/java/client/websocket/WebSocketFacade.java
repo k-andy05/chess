25e66c2a4c7e75a095f9fb4438016da1f38a3549
k-andy05 <kandrea1@byu.edu>
2026-04-06 17:06:38 -0600

    Client - WebSocketFacade written but not tested

---
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -0,0 +1,54 @@
+package client.websocket;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import jakarta.websocket.*;
+//import org.glassfish.grizzly.http.server.Session;
+//import org.glassfish.tyrus.core.wsadl.model.Endpoint;
+
+//import javax.management.Notification;
+//import java.io.IO;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void sendCommand(UserGameCommand userGameCommand) throws ResponseException {
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(userGameCommand));
+        } catch (IOException e) {
+            throw new ResponseException(ResponseException.Code.ServerError, e.getMessage());
+        }
+    }
+}
